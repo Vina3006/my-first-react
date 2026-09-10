a7bf7ead11e34b243d183e75a76815afd9148e65
Vina <309553689+Vina3006@users.noreply.github.com>
2026-09-10 06:24:17 +0530

    Practiced useState and State management

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import StudentInfo from "./studentInfo";
+import { useState } from "react";
 function App() {
-  return (
+  const [count, setCount] = useState(0);
+    
+   return (
     <div>
       <h1>My React learning profile</h1>
       <StudentInfo 
@@ -9,8 +12,13 @@ function App() {
       year={3}
       college="my college"
        />
+       <h2>Counter:{count}</h2>
+
+       <button onClick={() => setCount(count + 1) }>Increment</button> <br  />
+       <button onClick={() => setCount(count - 1) }>Decrement</button> <br />
+       <button onClick={() => setCount(0) }>Reset</button>
     </div>
   );
-}
 
+};
 export default App;
