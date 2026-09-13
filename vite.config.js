16a708cf415d7053920326f6becad86d5d6eb2c5
Vina3006 <309553689+Vina3006@users.noreply.github.com>
2026-09-13 12:09:27 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base:"/my-first-react/",
+  plugins: [react(), tailwindcss()]
 })
